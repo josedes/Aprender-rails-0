39f2a6341bd308789369c0a6a7349b41965161bc
josedes <josedes@gmail.com>
2014-02-04 17:48:39 -0430

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AprenderRails::Application.config.secret_key_base = '6b74584f4c772e63418001d98eb03fcd0b132f647fa99fb0fc7d3f7f5f5ec511edd3c8b9a2c80e9d79277a7da2cd9f30d8562c7b30fda8c56ec60445ed9e2a82'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AprenderRails::Application.config.session_store :cookie_store, key: '_aprender-rails_session'
